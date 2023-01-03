8cc379925762869fc7659425ffe2e7b94c115859
Anna Kubai <achakubai@gmail.com>
2023-01-03 18:54:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Get the modal
+let modal = document.getElementById('modalContent');
+// Get the button that opens the modal
+let btn = document.getElementById('modalBtn');
+// Get the <span> element that closes the modal
+let modalClose = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+modalClose.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks the button, open the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//counter Adults:
+let minusBtnAdults = document.getElementById("minus-btnAdults");
+let countAdults = document.getElementById("countAdults");
+let plusBtnAdults = document.getElementById("plus-btnAdults");
+
+let countNumAdults = 0;
+countAdults.innerHTML = countNumAdults;
+
+minusBtnAdults.addEventListener("click", () => {
+	if(countNumAdults > 0) {
+	countNumAdults -= 1;
+	} else {
+		countNumAdults = 0;
+	}
+	countAdults.innerHTML = countNumAdults;
+});
+
+plusBtnAdults.addEventListener("click", () => {
+	countNumAdults += 1;
+	countAdults.innerHTML = countNumAdults;
+});
+
+
+//counter Children
+let minusBtnChildren = document.getElementById("minus-btnChildren");
+let countChildren = document.getElementById("countChildren");
+let plusBtnChildren = document.getElementById("plus-btnChildren");
+
+let countNumChildren = 0;
+countChildren.innerHTML = countNumChildren;
+
+minusBtnChildren.addEventListener("click", () => {
+	if(countNumChildren > 0) {
+	countNumChildren -= 1;
+	} else {
+		countNumChildren = 0;
+	}
+	countChildren.innerHTML = countNumChildren;
+});
+
+plusBtnChildren.addEventListener("click", () => {
+	countNumChildren += 1;
+	countChildren.innerHTML = countNumChildren;
+});
